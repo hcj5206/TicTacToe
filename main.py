f12bf102e2a8006c3fe46d07b4db311253c54069
huangchenjian <huangchenjian@tju.edu.cn>
2019-08-21 16:38:21 +0800

    #20190821A 添加测试图片集

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 '''
-20190821A 建立主程序
+#20190821A 测试模式实现获取用户下子的位置和根据棋盘信息推算，AI选择最佳位置落子
 '''
-
+from game import Game
 if __name__ == '__main__':
-    GetPlayerPos()
+    Game=Game()
+    Game.start()
 
